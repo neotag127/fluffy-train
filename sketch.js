cc7b5342f65dcff40e2fc2eb5a4874c5cd4c4895
neotag127 <146630418+neotag127@users.noreply.github.com>
2023-11-08 17:55:24 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,117 +1,147 @@
-var garden,rabbit,apple,orangeL,redL;
-var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
 
+var knife,fruit ,monster,fruitGroup,monsterGroup, score,r,randomFruit, position;
+var knifeImage , fruit1, fruit2 ,fruit3,fruit4, monsterImage, gameOverImage;
+var gameOverSound ,knifeSwoosh;
 
 function preload(){
-  gardenImg = loadImage("garden.png");
-  rabbitImg = loadImage("rabbit.png");
-  appleImg = loadImage("apple.png");
-  orangeImg = loadImage("orangeLeaf.png");
-  redImg = loadImage("redImage.png");
+  
+  knifeImage = loadImage("knife.png");
+  monsterImage = loadAnimation("alien1.png","alien2.png")
+  fruit1 = loadImage("fruit1.png");
+  fruit2 = loadImage("fruit2.png");
+  fruit3 = loadImage("fruit3.png");
+  fruit4 = loadImage("fruit4.png");
+  gameOverImage = loadImage("gameover.png")
+  
+  gameOverSound = loadSound("gameover.mp3")
+  knifeSwooshSound = loadSound("knifeSwoosh.mp3")
 }
 
 
-function setup(){
+
+function setup() {
+  createCanvas(600, 600);
   
-  createCanvas(400,400);
-// Moving background
-garden=createSprite(200,200);
-garden.addImage(gardenImg);
+  //creating sword
+   knife=createSprite(40,200,20,20);
+   knife.addImage(knifeImage);
+   knife.scale=0.7
+  
+  
+  
+  //set collider for sword
+  knife.setCollider("rectangle",0,0,40,40);
 
-
-//creating boy running
-rabbit = createSprite(160,340,20,20);
-rabbit.scale =0.09;
-rabbit.addImage(rabbitImg);
+  // Score variables and Groups
+  score=0;
+  fruitGroup=createGroup();
+  monsterGroup=createGroup();
+  
 }
 
 function draw() {
-  background(0);
+  background("lightblue");
   
-  // boy moving on Xaxis with mouse'
-  rabbit.x = World.mouseX;
+  if(gameState===PLAY){
+    
+    //Call fruits and Monster function
+    fruits();
+    Monster();
+    
+    // Move sword with mouse
+    knife.y=World.mouseY;
+    knife.x=World.mouseX;
   
-  edges= createEdgeSprites();
-  rabbit.collide(edges);
-  
-   drawSprites();
+    // Increase score if sword touching fruit
+    if(fruitGroup.isTouching(knife)){
+      fruitGroup.destroyEach();
+      
+       knifeSwooshSound.play();
+       knifeSwooshSound.play;
+       knifeSwooshSound();
+       knifeSwooshSoundplay();
+
+
+       score=score;
+       score=+2;
+       score=2;
+       score=score+2;
+
+    }
+    else
+    {
+      // Go to end state if sword touching enemy
+      if(monsterGroup.isTouching(knife)){
+        gameState=END;
+        //gameover sound
+        gameOverSound.play()
+        
+        fruitGroup.destroyEach();
+        monsterGroup.destroyEach();
+        fruitGroup.setVelocityXEach(0);
+        monsterGroup.setVelocityXEach(0);
+        
+        // Change the animation of sword to gameover and reset its position
+        knife.addImage(gameOverImage);
+        knife.scale=2;
+        knife.x=300;
+        knife.y=300;
+      }
+    }
   }
+  
+  drawSprites();
+  //Display score
+  textSize(25);
+  text("Score : "+ score,250,50);
+}
+
+
+function Monster(){
+  if(World.frameCount%200===0){
+    monster=createSprite(400,200,20,20);
+    monster.addAnimation("moving", monsterImage);
+    monster.y=Math.round(random(100,550));
+    monster.velocityX=-(8+(score/10));
+    monster.setLifetime=50;
+    
+    monsterGroup.add(monster);
+  }
+}
+
+function fruits(){
+  if(World.frameCount%80===0){
+    fruit=createSprite(400,200,20,20);
+    fruit.x = 0    
+  //Increase the velocity of fruit after score 4 
+
+       fruit.velocityX= (7+(score/4));
+       fruit.velocityY= (7+(score));
+       fruit.velocity= (7+(score/4));
+       fruit.velocityX= (7);
+     
+    fruit.scale=0.2;
+     //fruit.debug=true;
+     r=Math.round(random(1,4));
+    if (r == 1) {
+      fruit.addImage(fruit1);
+    } else if (r == 2) {
+      fruit.addImage(fruit2);
+    } else if (r == 3) {
+      fruit.addImage(fruit3);
+    } else {
+      fruit.addImage(fruit4);
+    }
+    
+    fruit.y=Math.round(random(50,550));
    
-  
- var select_sprites = Math(random(1,3));
-
- var select_sprites = Math.random(random(1,3));
-
- var select_sprites = Math.round(1,3);
-
- var select_sprites = Math.round(random(1,3));
-
-  
-   if (frameCount % 50 === 0) {
-     if (select_sprites === 1) {
-      createApples();
-   } else if (select_sprites === 2) {
-       createOrange();
-     }else {
-       createRed();
-     }
-   }
-
-   if (frameCount % 80 === 0) {
-     if (select_sprites === 1) {
-       createApples();
-     } else if (select_sprites === 2) {
-       createOrange();
-     }
-  
-
-   if (frameCount / 80 === 0) {
-     if (select_sprites === 1) {
-       createApples();
-     } else if (select_sprites === 2) {
-       createOrange();
-     }else {
-       createRed();
-     }
-   }
-
-   if (frameCount % 80 === 0) {
-     if (select_sprites === 1) {
-       createApples();
-     } else if (select_sprites === 2) {
-       createOrange();
-   }else {
-      createRed();
-     }
-   }
-
-
-
-}
-
-function createApples() {
-apple = createSprite(random(50, 350),40, 10, 10);
-apple.addImage(appleImg);
-apple.scale=0.07;
-apple.velocityY = 3;
-apple.lifetime = 150;
-  
-}
-
-function createOrange() {
-orangeL = createSprite(random(50, 350),40, 10, 10);
-orangeL.addImage(orangeImg);
-orangeL.scale=0.08;
-orangeL.velocityY = 3;
-orangeL.lifetime = 150;
-}
-
-function createRed() {
-
-redL = createSprite(random(50, 350),40, 10, 10);
-redL.addImage(redImg);
-redL.scale=0.06;
-redL.velocityY = 3;
-redL.lifetime = 150;
-
+    
+    fruit.setLifetime=100;
+    
+    fruitGroup.add(fruit);
+  }
 }
